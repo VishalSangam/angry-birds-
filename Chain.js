b225c661572edee40691285aefbfd8cad2768a3a
VishalSangam <77846984+VishalSangam@users.noreply.github.com>
2021-04-15 20:55:27 +0530

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,27 @@
+class Chain{
+
+    constructor(bodyA,bodyB){
+        var options = {
+            bodyA: bodyA,
+            bodyB : bodyB,
+            length : 10,
+            stiffness : 0.015
+        }
+    
+        this.rope = Constraint.create(options);
+        World.add(world,this.rope);
+    
+    }
+    
+    display(){
+    
+        var pointA = this.rope.bodyA.position;
+        var pointB = this.rope.bodyB.position;
+    
+        strokeWeight(4);
+        line(pointA.x,pointA.y,pointB.x,pointB.y);
+    }
+    
+
+    
+    }
